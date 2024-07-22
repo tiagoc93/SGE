f82ece889923c853af6b2d8febc79074eded9588
tiago <t.c.s.andrade@hotmail.com>
2024-07-22 11:17:39 -0300

    implementação de base.html e componentes.

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import teste_base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', teste_base),
 ]
---
+++ app/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def teste_base(request):
+    return render(request,'base.html',)
